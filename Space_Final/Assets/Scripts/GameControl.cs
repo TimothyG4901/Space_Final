bd2479865f125e7fc174d86addf37d248c1f9938
Galindo <timothygalindo07@gmail.com>
2022-05-09 17:16:07 -0700

    Revert "recover"
    This reverts commit 970db8db1258c0bc4ce2824a92e9f0de4c167f7e.

--- Space_Final/Assets/Scripts/GameControl.cs
+++ Space_Final/Assets/Scripts/GameControl.cs
@@ -37,7 +37,7 @@ public class GameControl : MonoBehaviour
         }
     }
 
-    public void BirdScored()
+    public void PlayerScored()
     {
 
         if (gameOver)
@@ -46,7 +46,7 @@ public class GameControl : MonoBehaviour
         scoreText.text = "Score: " + score.ToString();
     }
 
-    public void BirdDied()
+    public void PlayerDied()
     {
 
         gameOvertext.SetActive(true);
